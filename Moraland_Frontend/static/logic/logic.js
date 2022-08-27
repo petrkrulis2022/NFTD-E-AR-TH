f486a8f110bb54f2259bbf62a5ef5789c8710041
Petar <p.todorov212@gmail.com>
2022-08-28 02:02:58 +0300

    initialised the project environment

---
+++ Moraland_Frontend/static/logic/logic.js
@@ -0,0 +1,10 @@
+
+
+
+//Graphics constants
+// - map management constants
+const tiles = 12;
+const plots = tiles * 9;
+const roads = tiles * 2;
+const initialOffsets = plots +roads;
+const plotViewOffsets = plots + (2 * roads);
