d9121617cea5d6ec283897f072ea9b0016c9d07f
Petar <p.todorov212@gmail.com>
2022-09-03 13:33:44 +0300

    converted info to an object and added IPFS uploading functionality

--- Moraland_Frontend/static/js/logic.js
+++ Moraland_Frontend/static/js/logic.js
@@ -2,151 +2,80 @@
 //Initializing constants
 const serverUrl = "https://ljgiacaiuzaf.usemoralis.com:2053/server";
 const appId = "6CU1DIgbkjrck51m2HCB8WB5ymwcgAxEk795qXWS";
-
-//Graphics constants
-// - map management constants
-const tiles = 12;
-const plots = tiles * 9;
-const roads = tiles * 2;
-const initialOffsets = plots +roads;
-const plotViewOffsets = plots + (2 * roads);
-
-//- canvas and context
-const mainCanvas = document.getElementById("mainCanvas");
-const mainCtx = mainCanvas.getContext('2d');
-const plotCanvas = document.getElementById("plotCanvas");
-const plotCtx = plotCanvas.getContext('2d');
-const worldImage = new Image();
-
-//- State
-const mapView = {mapOffsetX:-1*initialOffsets,mapOffsetY:-1*initialOffsets};
-const plotView = {plotX:0, plotY:0, locationX:0,locationY:0};
-const unassignables =  [ 
-                        "0x8fa5a0f171f8b06a2006bc8fa5164606fa303f29b43f25438ae585d32f979524",
-                        "0x51335e5c0d362b12bbc4877297443f914f0f9849da3bb43485ce6d96fcc6f9c9",
-                        "0xbb15fe082bb44a75feb18fcb4072d7e1d817f016d24cc1b1205c4f729e0b69ca",
-                        "0x5e2da10292384f87396bd6e706838dcb931f91404f3f01e4ef9538f3e161262e",
-                        "0x760a8a1da896267c45023b0d1c17a5bc42b81d12ab49a4bf1ccfec74c1ebe8ae",
-                        "0xad2572137374014c87d7abe24a591d3d65aee5cdc781ae15c727a971c5637aa7",
-                        "0xc2c26eb9355dec9ceae980da9bb626c7b583fe9af9c779333ea6e19309c2f3a6",
-                        "0xbf8142b3eb80adcbdb8a1c1a146995728a859ab1a1dbf29dcf15ebeb32f4a468",
-                        "0x3c02bccec034096f69ebbaacc075adbf352d2309a6de6b632d7b18b10b60180c",
-                        "0xfaf95d6bec5226ee145a021be8bda12c1062f7817ba6340499023b26d7e7a3f0",
-                        "0x98ce6651c1676f0d20fc0553d96755f2162f97c874ec668be5ebc68c3b2b7b41"
-                        ]
-
-//web3 constants
-const contractAddress = "0x65Faf7661789254858121630f99a489A54c7DbB1"; 
-const contractABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"tokenURI","type":"string"},{"internalType":"bytes","name":"bytesId","type":"bytes"}],"name":"assign","outputs":[],"stateMutability":"nonpayable","type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"},{"indexed":true,"internalType":"address","name":"assignee","type":"address"},{"indexed":false,"internalType":"bytes","name":"bytesId","type":"bytes"}],"name":"Assigned","type":"event"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes","name":"bytesId","type":"bytes"}],"name":"exist","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"}];
-
 const ethers = Moralis.web3Library;
 
 
-//canvas drawing functions
-function drawCanvas(){
-    mainCanvas.width = 3 * plots + 4 * roads;
-    mainCanvas.height = 3 * plots + 4 * roads;
-    plotCanvas.width = plots
-    plotCanvas.height = plots
-    worldImage.src = 'static/img/Moraland.png';
-    worldImage.onload = () => {
-        initializeMap()
-    }
-}
+//=======================================================================================================================================-
+//variables and dummy dataset
+//========================================================================================================================================
+let plotView =["void.snows.lobbingsight","faulting.akin.launchers","bumpkins.meeting.slalom","void.snows.lobbing","dilating.reciprocal.suitably"]
+let hashStore=[]    // an array that would store the hashed string of the  concatenated words for each tile with dots included
+let HashedArrAsString;   /// a string that represents all the hashes form every single tile,concatenated
+let plotID;               // will hold the master hash as a value;
+let incrementer =0;
+let nftObject;
 
-function initializeMap() {
-    updatePlotLocation()
-    setPlotData();
-    drawMapSection(mainCtx,mapView.mapOffsetX,mapView.mapOffsetY);
-    drawCursor(plotViewOffsets,plotViewOffsets);
-    drawMapSection(plotCtx,-1 * plotView.locationX,-1 *plotView.locationY);
-}
 
-//animate functions
-function move(direction) {
-    const validMove = validateMove(direction);
-    if (validMove) {
-        updateView(direction);
-        drawMapSection(mainCtx,mapView.mapOffsetX,mapView.mapOffsetY);
-        updatePlotLocation();
-        drawCursor(plotViewOffsets,plotViewOffsets);
-        drawMapSection(plotCtx,-1 * plotView.locationX,-1 *plotView.locationY);
-        setPlotData();
-    }
-}
+//=======================================================================================================================================
+HTML Selectors
+//=======================================================================================================================================-
+let $render=document.getElementById("render");   // not improtant 
+let $plotID=document.getElementById("plotID");   // not important
+let $ipfs=document.getElementById("ipfs");   // not important
 
-function validateMove(direction) {
-    switch(direction){
-        case 'ArrowRight': return !(plotView.plotX == 5);
-        case 'ArrowUp': return   !(plotView.plotY == 0);
-        case 'ArrowLeft': return !(plotView.plotX == 0);
-        case 'ArrowDown': return !(plotView.plotY == 5);
-    }
-}
-
-function updateView(direction) {
-    switch(direction){
-        case 'ArrowRight': 
-            plotView.plotX += 1;
-            mapView.mapOffsetX -= plots + roads;
-            break
-        case 'ArrowDown': 
-            plotView.plotY += 1;
-            mapView.mapOffsetY -= plots + roads;
-            break
-        case 'ArrowLeft': 
-            plotView.plotX -= 1;
-            mapView.mapOffsetX += plots + roads;
-            break
-        case 'ArrowUp': 
-            plotView.plotY -= 1;
-            mapView.mapOffsetY += plots + roads;
-            break
-    }
-}
-
-function drawMapSection(ctx,originX,originY){
-    ctx.drawImage(worldImage,originX,originY);
-}
-
-function drawCursor(x,y) {
-    mainCtx.strokeRect(x,y,plots,plots)
-}
-
-function updatePlotLocation() {
-    plotView.locationX = -1 * mapView.mapOffsetX + plotViewOffsets;
-    plotView.locationY = -1 * mapView.mapOffsetY + plotViewOffsets;
-}
-
+ 
 //UI Functions
 function setPlotData() {
-    const plotID = ethers.utils.id(JSON.stringify(plotView));
-    document.getElementById("plotX").value = plotView.plotX
-    document.getElementById("plotY").value = plotView.plotY
-    document.getElementById("locationX").value = plotView.locationX
-    document.getElementById("locationY").value = plotView.locationY
-    document.getElementById("plotID").value = plotID;
-    isPlotAssignable(plotID);
-}
- 
-function isPlotAssignable(plotID) {
-    const _unassignable = unassignables.includes(plotID);
-    if (_unassignable) {
-        document.getElementById("claimButton").setAttribute("disabled",null);
-    } 
-    else {
-        document.getElementById("claimButton").removeAttribute("disabled");
+    console.log(plotView);                      // displaying the initial words array
+    for (let i=0; i<plotView.length; i++){      //loop that invokes a keccak hash on every element in the tiles' words array
+      let hash = ethers.utils.id((plotView[i]));
+      hashStore.push(hash);                     // push the resulting hash into the hashstore array
     }
-} 
 
-function displayMessage(messageType, message){
-    const messages = {
-                "00":`<div class= "alert alert-success"> ${message} </div>`,
-                "01":`<div class= "alert alert-danger"> ${message} </div>`
-            }
-    document.getElementById("notifications").innerHTML = messages[messageType];
+    HashedArrAsString=hashStore.join("");                  // i convert the entira hasstore array to a single string
+     plotID = ethers.utils.id(HashedArrAsString);          // i pass this string to the hashing function again
+    console.log(plotID);
+    nftObject = Object.assign({},hashStore);             //initialising an object that would contain the hashes of all claimed tiles , as well as their masterhash
+    nftObject.masterHash=plotID;                          //masterhash
+    console.log(nftObject);
+    $plotID.value=plotID;
+   
+
+    /// code below not important 
+   plotView.map(tileWords=>$render.innerHTML+=`<span> Tile ${incrementer++} coordinates<input disabled="true" id=${incrementer} type="text" class="form-control" value=${tileWords}><p></p>`);
+
 }
 
+//========================================================================================================================================
+//IPFS functionality
+//========================================================================================================================================
+
+
+async function uploadToIpfs () {
+    const metadata = new Moralis.File("metadata.json", {
+        base64: btoa(JSON.stringify(nftObject)),
+      });
+      await metadata.saveIPFS();
+      console.log(metadata.hash());
+      console.log(metadata.ipfs());                     //check the console for the ipfs link
+}
+
+$ipfs.addEventListener('click',uploadToIpfs);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//========================================================================================================================================
 //web3 Functions
 async function login(){
     Moralis.Web3.authenticate().then(async function (){
@@ -154,7 +83,7 @@ async function login(){
     });
 }
 
-async function assignPlot() {
+/* *async function assignPlot() {
     const plotID = document.getElementById("plotID").value;
     const assigned = await isPlotAssigned(plotID);
     if (!assigned) {
@@ -175,6 +104,7 @@ async function assignPlot() {
         displayMessage("01","Plot is already assigned");
     }
 }
+
 
 async function mint(_tokenURI) {
     const contractOptions = {
@@ -210,10 +140,12 @@ async function isPlotAssigned(plotID) {
     return await Moralis.executeFunction(contractOptions);
 }
 
+*/
 
 Moralis.start({ serverUrl, appId }); 
 login(); 
-drawCanvas();
-window.addEventListener('keydown' , (e) => {
-    move(e.key)
-});
+setPlotData();
+//drawCanvas();
+//window.addEventListener('keydown' , (e) => {
+ //   move(e.key)
+//});
